c55b5a5f0286e89af0c6175c45a0806897703c36
studiobutter <bebosua05@yahoo.com.vn>
2025-03-17 23:41:56 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Starward Sleepy Gacha Log to UIGF v4.0 conversion
+
+This is a simple Python script that convert [Starward](https://github.com/Scighost/Starward)'s Sleepy Gacha Log to UIGFv4 schema for easy importing to other services like [Hoyo Buddy](https://github.com/seriaati/hoyo-buddy).
+
+This Script only work for version [0.14.0 Preview 1](https://github.com/Scighost/Starward/releases/tag/0.14.0-preview.1) and only work with the triple Z game's Gacha Log Function due to the early non-standard UIGF format.
+
+No plans for Train Game or Open World Anime Game
+
+Requires [Python](https://python.org)
+
+Open a terminal in the current folder path and run this command:
+
+```python convert.py```
+
+Follow the instruction to convert the file.
---
+++ convert.py
@@ -0,0 +1,59 @@
+import json
+import os
+
+def convert_rank_type(rank_type):
+    return str(int(rank_type) + 1)
+
+def convert_source_to_target(source_path, target_path):
+    with open(source_path, 'r') as source_file:
+        source_data = json.load(source_file)
+
+    target_data = {
+        "info": {
+            "export_timestamp": source_data["info"]["export_timestamp"],
+            "export_app": source_data["info"]["export_app"],
+            "export_app_version": source_data["info"]["export_app_version"],
+            "version": "v4.0"
+        },
+        "nap": [
+            {
+                "uid": source_data["info"]["uid"],
+                "timezone": source_data["info"]["region_time_zone"],
+                "list": []
+            }
+        ]
+    }
+
+    for item in source_data["list"]:
+        target_item = {
+            "id": item["id"],
+            "uigf_gacha_type": item["gacha_type"],
+            "gacha_type": item["gacha_type"],
+            "item_id": item["item_id"],
+            "time": item["time"],
+            "rank_type": convert_rank_type(item["rank_type"])
+        }
+        target_data["nap"][0]["list"].append(target_item)
+
+    source_filename = os.path.splitext(os.path.basename(source_path))[0]
+    target_filename = f"UIGFv4_{source_filename}.json"
+    if os.path.isdir(target_path):
+        target_path = os.path.join(target_path, target_filename)
+    else:
+        target_path = target_filename
+
+    with open(target_path, 'w') as target_file:
+        json.dump(target_data, target_file, indent=4)
+
+if __name__ == "__main__":
+
+    source_path = input("Enter the source file: ").strip()
+    target_path = input("Enter the target path (leave empty to save in the current directory): ").strip()
+    if not target_path:
+        target_path = "."
+
+    if not os.path.isfile(source_path):
+        print(f"Source file '{source_path}' does not exist.")
+    else:
+        convert_source_to_target(source_path, target_path)
+        print(f"Conversion completed. Target file saved at '{target_path}'.")
